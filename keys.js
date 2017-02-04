e323d4739c562399620e20ec7dd51de31ada320a
aaronjaymus <aaronjaymus@gmail.com>
2017-02-04 17:31:12 -0500

    Initial file add

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '15rkPrg7O5dCj7EUhPl04mqax',
+  consumer_secret: 'Pt2DLVktbZo3Ljy6P0Ua91pxHaoDEEo0IYZ8fdazf5lKrp4jlV',
+  access_token_key: '59645106-zWBNXfX2j66Yhq90TlkuKh5DPuAohDDpewRXlZ0mp',
+  access_token_secret: 'CBKlno8PmndswSx3kC7azO9P5Jn3kYNtJzbpkyVt2vqlx',
+}
